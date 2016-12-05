e029680fe8cefc8fa59221bb4d2e8f6c115e01da
evgeny-kravchuk <kravchuk20111996@gmail.com>
2016-12-05 22:25:48 +0200

    Add observer class

---
+++ CourseProject/Observer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    public interface IObserver
+    {
+        void Subscribe(IObserver observer);
+        void Unsubscribe(IObserver observer);
+        void NotifyObservers();
+        void Update();
+    }
+}
