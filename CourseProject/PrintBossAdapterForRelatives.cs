ede99496be260b5d4f0c15e752aabe0dc3aa7a36
evgeny-kravchuk <kravchuk20111996@gmail.com>
2016-12-05 22:37:47 +0200

    Added some tests

---
+++ CourseProject/PrintBossAdapterForRelatives.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    class PrintBossAdapterForRelatives : EmployeeComponent
+    {
+        private PersonComponent _personComponent;
+
+        public PrintBossAdapterForRelatives(PersonComponent personComponent)
+        {
+            _personComponent = personComponent;
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return _personComponent.Name;
+            }
+            set { }
+        }
+
+        public override void Lead()
+        {
+            _personComponent.Children();
+        }
+
+        public override void Salary()
+        {
+            _personComponent.Heirs();
+        }
+
+        public override void Rise()
+        {
+        }
+    }
+}
